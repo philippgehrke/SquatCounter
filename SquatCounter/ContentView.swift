bd564b1a953b0ced8c7e95e504786e0063bc0c4c
philippgehrke <37164282+philippgehrke@users.noreply.github.com>
2021-06-04 12:20:26 +0200

    Initial Commit

---
+++ SquatCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SquatCounter
+//
+//  Created by Jan Philipp Gehrke on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SquatCounter/SquatCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquatCounterApp.swift
+//  SquatCounter
+//
+//  Created by Jan Philipp Gehrke on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SquatCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
